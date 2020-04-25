06614ba6575b050327e368c2b23d999a062dbf22
deehrlic <deehrlic@ucsc.edu>
2020-04-24 23:49:29 -0700

    step before mongo

--- flaskbrr.py
+++ flaskbrr.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+import re
+import requests
+#from bs4 import BeautifulSoup
+import wikipedia, random
+import urllib.request
 
 app = Flask(__name__)
 
@@ -9,9 +14,50 @@ def home():
 @app.route("/parse", methods=['GET', 'POST'])
 def parse():
     if request.method == "POST":
-        return request.form['what']
 
         #make verbose
+        #you cant just make -synonym/hypernym- machine go brrrrr
+        verbose = "NOOOOOOOOOOOOOOO!!! You can't just make " + request.form['user_i'] + " go brrrrr!"
+
+
+        #get image from wikipedia and save to computer
+        res = wikipedia.search(request.form['user_i'])
+        img = ""
+        if len(res) > 0:
+            try:
+                img = wikipedia.page(res[0]).images[0]
+                print(img)
+                path = "static/"+res[0].replace(" ","")+".jpg"
+
+            except wikipedia.DisambiguationError as e:
+                img = wikipedia.page(e.options[0]).images[0]
+                print(img)
+                path = "static/"+e.options[0].replace(" ","")+".jpg"
+
+            urllib.request.urlretrieve(img, path)
+
+                #SEND TO MONGO
+                #request.form['user_i'] = input term
+                #verbose = verbose form of term
+                #img = the link to image to be displayed
+
+            #change to PIL image
+            return send_file(path, mimetype='image')
+
+
+        #THIS MEANS SEARCH FAILED BRO
+        else:
+            return 'sdfsdfds'
+
+
+
+
+
+
+
+        return verbose
+
+
         #get image
         #send to mongo
         #assemble image
